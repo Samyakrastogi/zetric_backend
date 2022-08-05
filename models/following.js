de555118dffa51d504010f748d1c6f0185d43ac6
Samyakrastogi <samyak.rastogi_cs19@gla.ac.in>
2022-08-05 14:44:45 +0530

    commit

--- models/following.js
+++ models/following.js
@@ -6,10 +6,10 @@ const followingAndFollowerSchema = new mongoose.Schema({
     ref: "Users",
     required: true,
   },
-  followUserId: { type: String, required: true }, //id to user to be followed
-  userName: { type: String, required: true }, //followed username
+  followUserId: { type: String, required: true }, //id of user to be followed
+  userName: { type: String, required: true }, //username of id to be followed
   profileImageURL : String
-                   // Maybe add username as a link to redirect to the specific profile
+        // Maybe add username as a link to redirect to the specific profile
 });
 
 
